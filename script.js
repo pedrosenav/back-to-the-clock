eac1d2d338c198bb503ff99824c41c256004afb9
Pedro Sena <vizeufernando@gmail.com>
2022-08-07 14:50:21 -0300

    Relógio atualiza automaticamente!
    Todo o script foi envelopado em uma função e foi setado um intervalo para o horário atualizar

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function monthName(m){
+function clock(){
+    function monthName(m){
     switch(m){
         case 1:
             m = 'JAN'
@@ -43,33 +44,36 @@ function monthName(m){
         return m;
     }
 
-var date = new Date()
-var year = date.getFullYear()
-var month = monthName(date.getMonth()+1)
-var day = date.getDate()
-    day = day<10?'0'+day:day
-var hour = date.getHours()
-    hour = hour<10?'0'+hour:hour
-var minute = date.getMinutes()
-    minute = minute<10?'0'+minute:minute
+    var date = new Date()
+    var year = date.getFullYear()
+    var month = monthName(date.getMonth()+1)
+    var day = date.getDate()
+        day = day<10?'0'+day:day
+    var hour = date.getHours()
+        hour = hour<10?'0'+hour:hour
+    var minute = date.getMinutes()
+        minute = minute<10?'0'+minute:minute
 
-var clockYear = document.querySelector('div.year span')
-var clockMonth = document.querySelector('div.month span')
-var clockDay = document.querySelector('div.day span')
-var clockHour = document.querySelector('div.hour span')
-var clockMinute = document.querySelector('div.minute span')
+    var clockYear = document.querySelector('div.year span')
+    var clockMonth = document.querySelector('div.month span')
+    var clockDay = document.querySelector('div.day span')
+    var clockHour = document.querySelector('div.hour span')
+    var clockMinute = document.querySelector('div.minute span')
 
-clockYear.innerHTML = year
-clockMonth.innerHTML = month
-clockDay.innerHTML = day
-clockHour.innerHTML = hour
-clockMinute.innerHTML = minute
+    clockYear.innerHTML = year
+    clockMonth.innerHTML = month
+    clockDay.innerHTML = day
+    clockHour.innerHTML = hour
+    clockMinute.innerHTML = minute
 
-var am = document.querySelector('div.am > div')
-var pm = document.querySelector('div.pm > div')
+    var am = document.querySelector('div.am > div')
+    var pm = document.querySelector('div.pm > div')
 
-if(hour>=0 && hour<=12){
-    pm.style.backgroundColor = '#115b01'
-}else{
-    am.style.backgroundColor = '#115b01'
+    if(hour>=0 && hour<=12){
+        pm.style.backgroundColor = '#115b01'
+    }else{
+        am.style.backgroundColor = '#115b01'
+    }
+
 }
+setInterval(clock,50000)
