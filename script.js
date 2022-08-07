b7535d90d11d6bc80045477ed4d11a30f30675a8
Pedro Sena <vizeufernando@gmail.com>
2022-08-07 04:00:48 -0300

    Clock created!

---
+++ script.js
@@ -0,0 +1,67 @@
+function monthName(m){
+    switch(m){
+        case 1:
+            m = 'JAN'
+            break;
+        case 2:
+            m = 'FEB'
+            break;
+        case 3:
+            m = 'MAR'
+            break;
+        case 4:
+            m = 'APR'
+            break;
+        case 5:
+            m = 'MAY'
+            break;
+        case 6:
+            m = 'JUN'
+            break;
+        case 7:
+            m = 'JUL'
+            break;
+        case 8:
+            m = 'AUG'
+            break;
+        case 9:
+            m = 'SEP'
+            break;
+        case 10:
+            m = 'OCT'
+            break;
+        case 11:
+            m = 'NOV'
+            break;
+        case 12:
+            m = 'DEC'
+            break;
+        default:
+            m = 'error'
+
+        }
+        return m;
+    }
+
+var date = new Date()
+var year = date.getFullYear()
+var month = monthName(date.getMonth()+1)
+var day = date.getDate()
+    day = day<10?'0'+day:day
+var hour = date.getHours()
+    hour = hour<10?'0'+hour:hour
+var minute = date.getMinutes()
+    minute = minute<10?'0'+minute:minute
+//var ampm = ''
+
+var clockYear = document.querySelector('div.year span')
+var clockMonth = document.querySelector('div.month span')
+var clockDay = document.querySelector('div.day span')
+var clockHour = document.querySelector('div.hour span')
+var clockMinute = document.querySelector('div.minute span')
+
+clockYear.innerHTML = year
+clockMonth.innerHTML = month
+clockDay.innerHTML = day
+clockHour.innerHTML = hour
+clockMinute.innerHTML = minute
